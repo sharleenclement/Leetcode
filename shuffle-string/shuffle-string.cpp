81a429edf66756ddcacb4a0bdfe068a66e5955e0
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2021-07-17 11:46:41 +0530

    Time: 4 ms (95.25%), Space: 15.1 MB (75.21%) - LeetHub

--- shuffle-string/shuffle-string.cpp
+++ shuffle-string/shuffle-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
+        /*
         int i, j, n, k=0;
         n = indices.size();
         string str;
         for(i = 0; i < n; ++i)
         {
-            
             for(j = 0; j < n; ++j)
             {
                 if(indices[j]!=-1 && indices[j]==k)
@@ -18,5 +18,36 @@ public:
             }
         }
         return str;
+        TC: O(N^2), SC: O(N)
+        */
+        
+        /*
+        int i, n;
+        n = indices.size();
+        string str;
+        str.resize(n); // str = s;
+        for(i=0;i<n;++i)
+        {
+            str[indices[i]] = s[i];
+        }
+        return str;
+        TC: O(N), SC: O(N)
+        */
+        
+        int i = 0, n;
+        n = indices.size();
+        while(i!=n) // using while loop because we are not incrementing the loop all the time.
+        {
+            if(indices[i]==i)
+            {
+                i++; // only incrementing when the value is equal
+            }
+            else
+            {
+                swap(s[i], s[indices[i]]);
+                swap(indices[i], indices[indices[i]]);
+            }
+        }
+        return s;
     }
 };
