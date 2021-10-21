0894e2cb687f693bb150aa5d86167af1b6905464
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2021-10-22 00:10:05 +0530

    Time: 340 ms (10.14%), Space: 63 MB (47.84%) - LeetHub

--- find-the-town-judge/find-the-town-judge.cpp
+++ find-the-town-judge/find-the-town-judge.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        multimap<int,int>mp;
-        map<int,int>mp2;
-        int k=n-1;
-        int y=0;
-        if(n==1)
+       map<int,int>mp;
+        map<int,int>mp1;
+        if(trust.size()==0 && n==1)
             return 1;
+        if(n==1 || trust.size()==0)
+            return -1;
         for(int i=0;i<trust.size();i++)
         {
             mp.insert({trust[i][0],trust[i][1]});
+            mp1[trust[i][1]]++;
         }
-        for(auto j:mp)
+        vector<int>v;
+        for(auto i:mp1)
         {
-            // cout << "j " << j.first << " " << j.second << endl;
-            mp2[j.second]++;
+          v.push_back(i.second);  
         }
-        for(auto x:mp2)
-        {
-            // cout << "x " << x.first << " "  << x.second << endl;
-            if(x.second==k)
-              y=x.first;  
-        }
-        if(y==0)
+        int mx=*max_element(v.begin(),v.end());
+        if(mx!=n-1)
             return -1;
-        int c=0;
-        for(int i=1;i<=n;++i)
+        int k;
+        for(auto i:mp1)
         {
-            // cout << mp.count(i) << endl;
-            if(mp.count(i)>0)
-                c++;
+            if(i.second==mx)
+               k=i.first; 
         }
-        // cout << c << "  fin " << n << endl;
-        if(c==n)
-            return -1;
-        for(auto itr:mp)
+        for(auto i:mp)
         {
-            if(mp.count(y)==1)
-               return -1;
+          if(i.first==k)
+              return -1;
         }
-        return y;
+        return k;
     }
 };
