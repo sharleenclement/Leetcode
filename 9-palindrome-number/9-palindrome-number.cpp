4502ff1cdd8b3a8083c18866570d703937f5f45f
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-02-11 13:06:18 +0530

    Time: 8 ms (87.44%), Space: 5.8 MB (74.68%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0)
-            return false;
-        else if(x==0)
-            return true;
-        int d;
-        string ans="";
-        string k = to_string(x);
-        while(x!=0)
-        {
-            d = x%10;
-            ans += "" + to_string(d);
-            x = x/10;
-        }
-        if(ans==k)
-        return true;
-        
-        return false;
+        string s1 = to_string(x);
+        string s2 = s1;
+        reverse(s2.begin(), s2.end());
+        return s1==s2;
     }
 };
