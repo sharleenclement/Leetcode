d7dbd280e2bbf94f7c5f6bf7672c5e56cea7c726
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-02-22 16:24:42 +0530

    Time: 20 ms (43.25%), Space: 8.3 MB (23.11%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    int romanToInt(string s)
-    {
-        int n = s.size();
-        unordered_map<char, int> mapp;
-
-        mapp.insert({'I', 1});
-        mapp.insert({'V', 5});
-        mapp.insert({'X', 10});
-        mapp.insert({'L', 50});
-        mapp.insert({'C', 100});
-        mapp.insert({'D', 500});
-        mapp.insert({'M', 1000});
-
-        int ans = 0;
-
-        for (int i = n - 1; i >= 0; --i)
+    int romanToInt(string s) {
+        unordered_map<char,int>mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+        int sum=0;
+        int i=0;
+        while(i<s.length())
         {
-            if(s[i]=='V' and i>0 and s[i-1]=='I'){
-                ans+=4;--i;}
-
-            else if(s[i]=='X' and i>0 and s[i-1]=='I'){
-                ans+=9;--i;}
-
-            else if(s[i]=='L' and i>0 and s[i-1]=='X'){
-                ans+=40;--i;}
-
-            else if(s[i]=='C' and i>0 and s[i-1]=='X'){
-                ans+=90;--i;}
-
-            else if(s[i]=='D' and i>0 and s[i-1]=='C'){
-                ans+=400;--i;}
-
-            else if(s[i]=='M' and i>0 and s[i-1]=='C'){
-                ans+=900; --i;}
-
+            if(mp[s[i]]<mp[s[i+1]])
+               {
+                sum+=mp[s[i+1]]-mp[s[i]];
+                i=i+2;
+            }
             else
-                ans += mapp[s[i]];
+            {
+                 sum+=mp[s[i]];i++;
+            }
+        
         }
-
-        return ans;
+        return sum;
     }
 };
