7bb9b137a5951a2ff6a52da0a39ddb128e918698
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-02-23 11:24:18 +0530

    Time: 56 ms (5.13%), Space: 48.7 MB (5.48%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string> S) {
-        if (S.size() == 0) 
-            return "";
-        for (int i = 0; i < S[0].length() ; i++)
-        {
-            char c = S[0][i];
-            for (int j = 1; j < S.size(); j++) 
-            {
-                if (i == S[j].length() || S[j][i] != c)
-                    return S[0].substr(0, i);             
-            }
-        }
-        return S[0];
+ string commonPrefixUtil(string str1, string str2)
+{
+    string result;
+    int n1 = str1.length(), n2 = str2.length();
+ 
+    for (int i=0, j=0; i<=n1-1&&j<=n2-1; i++,j++)
+    {
+        if (str1[i] != str2[j])
+            break;
+        result.push_back(str1[i]);
     }
+    return (result);
+}
+string longestCommonPrefix1(vector<string> S, int low, int high)
+{
+    if (low == high)
+        return (S[low]);
+ 
+    if (high > low)
+    {
+        int mid = low + (high - low) / 2;
+ 
+        string str1 = longestCommonPrefix1(S, low, mid);
+        string str2 = longestCommonPrefix1(S, mid+1, high);
+ 
+        return (commonPrefixUtil(str1, str2));
+    }
+    return "";
+}
+    string longestCommonPrefix(vector<string> v)
+    {
+        int low = 0, high = v.size()-1;
+        return longestCommonPrefix1(v, low, high);
+    }
+
 };
