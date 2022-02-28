0a6989dcc77f011ee89d745d8ac8e53d52ed32cf
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-03-01 00:08:00 +0530

    Time: 16 ms (26.09%), Space: 14.7 MB (83.05%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,52 +1,33 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     
     ListNode* mergeTwoLists(ListNode* first, ListNode* second) {
-        // We will create a dummy node 
-    if(first == NULL)
-        return second;
-    if(second == NULL)
-        return first;
-    ListNode* output = NULL;
-    if(first->val < second->val)
-    {
-        output = first;
-        first = first->next;
+        ListNode* f = first;
+		ListNode* s = second;
+		ListNode* dummy = new ListNode(-1);
+		ListNode* fin = dummy;
+		
+		while(f!=NULL && s!=NULL)
+		{
+			if(f->val < s->val)
+			{
+				fin->next = f;
+				f = f->next;
+			}
+			else
+			{
+				fin->next = s;
+				s = s->next;
+			}
+			fin = fin->next;
+		}
+		
+		if(f!=NULL)
+		fin->next = f;
+		
+		if(s!=NULL)
+		fin->next = s;
+		
+		return dummy->next;
     }
-    else
-    {
-        output = second;
-        second = second->next;
-    }
-    ListNode* curr = output;
-    while(first != NULL && second!= NULL)
-    {
-        if(first->val > second->val)
-        {
-            curr->next = second;
-            second = second->next;
-        }
-        else
-        {
-            curr->next = first;
-            first = first->next;
-        }
-        curr = curr->next;
-    }
-    if(first!= NULL)
-        curr->next = first;
-   if(second!= NULL)
-        curr->next = second;
-   return output;
-}
 };
