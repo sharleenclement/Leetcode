8a374f04c17a26c54f213a4704e9cbecda76697a
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-03-08 09:07:40 +0530

    Time: 57 ms (41.47%), Space: 71.4 MB (81.65%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -13,16 +13,19 @@ public:
     
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     int c = 0;
-    ListNode newHead(0);
-    ListNode *t = &newHead;
+    // ListNode newHead(0);
+    ListNode *t = new ListNode();
+    ListNode *head = t;
+
     while(c || l1 || l2) {
         c += (l1? l1->val : 0) + (l2? l2->val : 0);
-        t->next = new ListNode(c%10);
-        t = t->next;
+        head->next = new ListNode(c%10);
+        head = head->next;
         c /= 10;
         if(l1) l1 = l1->next;
         if(l2) l2 = l2->next;
     }
-    return newHead.next;
-}
+        
+    return t->next;
+    }
 };
