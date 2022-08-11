453833529f5e2ee9500d82e0df0535eca2e10a11
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-08-12 04:16:46 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (100.00%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -5,10 +5,6 @@ public:
         while(n>1)
         {
             sort(stones.begin(), stones.end());
-            // for(j=0;j<n;++j)
-            // {
-            //     cout << stones[j] << " ";
-            // }
             if(stones[n-i-1]==stones[n-i-2])
             {
                 stones.pop_back();
@@ -20,18 +16,12 @@ public:
                 temp = stones[n-i-2];
                 stones.erase(stones.end()-2);
                 n--;
-                // cout << temp << endl;
                 stones[n-i-1] -= temp;
             }
         }
-        // for(j=0;j<n;++j)
-        // {
-        //     cout << stones[j] << " ";
-        // }
         if(n==0)
             return 0;
         
         return stones[i];
     }
-}; // 1 1 2 4 1
-// 1 1 1 2 4
+};
