f398aad8393d101998742a156c78a9ed972ea145
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-08-30 01:43:30 +0530

    Time: 18 ms (76.43%), Space: 13.1 MB (95.79%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,36 +2,34 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& arr) {
         int i, j, n = arr.size(), m = arr[0].size();
-    
-        int mat1[n], mat2[m];
-
-        for(i=0;i<n;++i)
-        mat1[i] = INT_MAX;
-
-        for(i=0;i<m;++i)
-        mat2[i] = INT_MAX;
-
+        bool col = true;
         for(i=0;i<n;++i)
         {
-            for(j=0;j<m;++j)
+            if(arr[i][0]==0)
+            col = false;
+            for(j=1;j<m;++j)
             {
                 if(arr[i][j]==0)
                 {
-                    mat1[i] = 0;
-                    mat2[j] = 0;
+                    arr[i][0] = 0;
+                    arr[0][j] = 0;
                 }
             }
         }
 
-        for(i=0;i<n;++i)
+        for(i=n-1;i>=0;--i)
         {
-            for(j=0;j<m;++j)
+            for(j=m-1;j>=1;--j)
             {
-                if(mat1[i]==0 or mat2[j]==0)
+                if(arr[i][0]==0 or arr[0][j]==0)
                 {
                     arr[i][j] = 0;
                 }
             }
+            if(col==false)
+            arr[i][0] = 0;
         }
+
+        
     }
 };
