57a6efd93b50b9159551d85c6259191709d2ea1f
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-08-31 03:08:22 +0530

    Time: 41 ms (5.48%), Space: 8.1 MB (8.62%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {        
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        int i, n=s.length(), m=t.length();
         
-        return s==t;
+        if(n!=m)
+            return false;
+        
+        priority_queue<char> q1;
+        priority_queue<char> q2;
+        
+        for(i=0;i<n;++i)
+        {
+            q1.push(s[i]);
+        }
+        
+        for(i=0;i<m;++i)
+        {
+            q2.push(t[i]);
+        }
+        
+        while(!q1.empty())
+        {
+            if(q1.top()!=q2.top())
+                return false;
+            q1.pop();
+            q2.pop();
+        }
+        
+        return true;
     }
 };
