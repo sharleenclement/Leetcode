a083bb5c1cf27534c97dd5e18aecdfda4c6f9c4e
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-08-30 22:18:36 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (90.76%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int z=0, o=0, t=0, n = nums.size(), i;
+        for(i=0;i<n;++i)
+        {
+            if(nums[i]==0)
+                z++;
+            else if(nums[i]==1)
+                o++;
+            else
+                t++;
+        }
+        
+        for(i=0;i<z;++i)
+            nums[i] = 0;
+        for(i=z;i<z+o;++i)
+            nums[i] = 1;
+        for(i=z+o;i<n;++i)
+            nums[i] = 2;
     }
 };
