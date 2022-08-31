66fb88e0684dcb36238ba01722e6a26a775c47d8
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-08-31 19:47:59 +0530

    Time: 3 ms (64.36%), Space: 7.1 MB (33.42%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -12,7 +12,10 @@ public:
         }
         for(i=0;i<n;++i)
         {
-            reverse(matrix[i].begin(), matrix[i].end());
+            for(j=0;j<n/2;++j)
+            {
+                swap(matrix[i][j], matrix[i][n-j-1]);
+            }
         }
     }
 };
