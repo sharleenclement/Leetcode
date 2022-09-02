76226e042e7e5e75a9b13175edbb5c0ca79ae780
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-09-02 14:19:11 +0530

    Time: 30 ms (58.56%), Space: 17.9 MB (64.58%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -3,16 +3,12 @@ public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int i, n = nums.size();
-        vector<int> vec;
-        for(i=0;i<=n;++i)
-        {
-            vec.push_back(i);
-        }
+
         for(i=0;i<n;++i)
         {
-            if(vec[i]!=nums[i])
+            if(i!=nums[i])
                 break;
         }
-        return vec[i];
+        return i;
     }
 };
