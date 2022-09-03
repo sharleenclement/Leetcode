6b662e2b4ef42b0c4f528715c4674ecac9c93541
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-09-04 00:02:04 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.18%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        double ans = 1.0, neg = 1.0;
+
+        if(n==0 and (ceil(x) == floor(x)) or x==1.0) // handle the decimal points
+            return x;
+        else if(n==0 and (ceil(x) != floor(x)))
+            return ans;
+        
+        long k = abs(n);
+        
+        while(k>0)
+        {
+            if(k%2==1)
+            {
+                ans = ans * x;
+                k--;
+            }
+            else
+            {
+                x = x*x;
+                k /= 2;
+            }
+        }
+        
+        if(n>0)
+            return ans;
+        
+        return 1.0/ans;
     }
 };
