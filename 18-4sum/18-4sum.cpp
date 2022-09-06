9f8ace26e0c39ea254a620b71a9fd60037b7b540
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-09-06 08:02:43 +0530

    Time: 70 ms (79.83%), Space: 13.1 MB (93.31%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,43 +1,53 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int i, j, k, n = nums.size(), s, e, mid;
-        long ans = 0;
-        set<vector<int>> svec;
+        int i, j, k, n = nums.size(), left, right;
+        vector<vector<int>> vec;
+        
+        if(n==0)
+            return vec;
+        
+        long target1 = 0, target2 = 0, fin = 0;
         sort(nums.begin(), nums.end());
+        
         for(i=0;i<n;++i)
         {
+            target1 = target - nums[i];
             for(j=i+1;j<n;++j)
             {
-                for(k=j+1;k<n;++k)
+                target2 = target1 - nums[j];
+                left=j+1, right=n-1;
+                while(left<right)
                 {
-                    vector<int> v;
-                    ans = (long)nums[i] + (long)nums[j] + (long)nums[k];
-                    s = k+1;
-                    e = n-1;
-                    while(s<=e)
+                    fin = (long)nums[left] + (long)nums[right];
+                    if(fin<target2)
+                        left++; 
+                    else if(fin>target2)
+                        right--; 
+                    else
                     {
-                        mid = s + (e-s)/2;
-                        if(nums[mid]+ans==target)
-                        {
-                            v.push_back(nums[i]);
-                            v.push_back(nums[j]);
-                            v.push_back(nums[k]);
-                            v.push_back(nums[mid]);
-                            sort(v.begin(),v.end());
-                            svec.insert(v);
-                            break;
-                        }
-                        else if(nums[mid]+ans<target)
-                            s = mid + 1;
-                        else
-                            e = mid - 1;
+                        vector<int> v(4, 0);
+                        v[0] = nums[i];
+                        v[1] = nums[j];
+                        v[2] = nums[left];
+                        v[3] = nums[right];
+                        vec.push_back(v);
+                        
+                        while(left<right && nums[left]==v[2]) 
+                            ++left;
+                        
+                        while(left<right && nums[right]==v[3]) 
+                            --right;
+
                     }
                 }
+                while(j+1<n && nums[j+1]==nums[j]) 
+                    ++j;
             }
+            while(i+1<n && nums[i+1]==nums[i]) 
+                ++i;
         }
-        vector<vector<int>> vec(svec.begin(), svec.end());
-        
+    
         return vec;
     }
 };
