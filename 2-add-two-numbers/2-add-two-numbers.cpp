d27f31b8d620e8484fd4eb22f6ce97ce37d3045c
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2022-09-08 13:27:55 +0530

    Time: 69 ms (28.49%), Space: 71.6 MB (12.68%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,22 +10,50 @@
  */
 class Solution {
 public:
-    
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-        if(!l1 && !l2)
-            return NULL;
-    
-        int c = (l1? l1->val : 0) + (l2? l2->val : 0);
-        ListNode* t = new ListNode(c%10);
-        ListNode* head = l1? l1->next : NULL;
-        c = c/10;
-        if(head) 
-            head->val += c;
-        else if(c) 
-            head = new ListNode(c);
-        t->next = addTwoNumbers(l2? l2->next:NULL, head);
-        
-        return t;
+        int carry=0, value=0, ans=0;
+        ListNode* l = l1;
+        ListNode* prev;
+        ListNode* n = new ListNode(0);
+        ListNode* fin = n;
+        while(l1!=NULL and l2!=NULL)
+        {
+            ans = l1->val + l2->val + carry;// 99 99 = 19 8
+            carry = ans/10;
+            value = ans%10;
+            ListNode* k = new ListNode(value);
+            l1 = l1->next;
+            l2 = l2->next;
+            n->next = k;
+            n = n->next;
+        }
+        while(l1!=NULL)
+        {
+            prev = l1;
+            ans = l1->val + carry;
+            carry = ans/10;
+            value = ans%10;
+            ListNode* k = new ListNode(value);
+            n->next = k;
+            n = n->next;
+            l1 = l1->next;
+        }
+        while(l2!=NULL)
+        {
+            prev = l2;
+            ans = l2->val + carry;
+            carry = ans/10;
+            value = ans%10;
+            ListNode* k = new ListNode(value);
+            n->next = k;
+            n = n->next;
+            l2 = l2->next;
+        }
+        if(carry>0)
+        {
+            ListNode* k = new ListNode(carry);
+            n->next = k;
+        }
+        return fin->next;
     }
 };
